54b19e621e7c64c2076406a8a506d7ed7e4b4aaa
MartinMxn <xuanningmeng@XuanningdeMacBook-Pro.local>
2019-01-18 19:36:58 -0800

    image

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -65,7 +65,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            ProgrammerConnector
+            DevConnector
           </Link>
           <button
             className="navbar-toggler"
